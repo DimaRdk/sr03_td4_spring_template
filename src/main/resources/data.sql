5a9d41a03be0d091ceaf20ba0a9f618f50556126
Florestan Biaux <biauxflorestan@gmail.com>
2023-06-05 11:20:00 +0200

    fix : ajout de react

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sr03_users (DTYPE,  firstname, lastName, mail, password, admin, is_active) VALUES ('Admin' , 'John', 'BOB', 'jb@test.com', 'jb', 1,1);
-INSERT INTO sr03_users (DTYPE, firstname, lastName, mail, password, admin, is_active) VALUES ('NormalUser', 'Paul', 'FIOUL', 'pf@test.com', 'pf', 0,1);
-INSERT INTO sr03_users (DTYPE, firstname, lastName, mail, password, admin, is_active) VALUES ('Admin', 'Michal', 'POULPE', 'mp@test.com', 'mp', 0,1);
-INSERT INTO sr03_users (DTYPE, firstname, lastName, mail, password, admin, is_active) VALUES ('Admin', 'test', 'test', 'test@test', 'test12345', 1,1);
